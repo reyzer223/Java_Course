7023b16549f16d079f880d3ef8e02f51f68f9437
Reyzer223 <reyzerlg@inbox.ru>
2023-09-05 20:35:24 +0600

    log fix

---
+++ Java_lesson/java2/logger.java
@@ -0,0 +1,15 @@
+import java.util.logging.*;
+public class logger{
+ public static void main(String[] args) {
+ Logger logger = Logger.getLogger(Ex0043.class.getName());
+ logger.setLevel(Level.INFO);
+ ConsoleHandler ch = new ConsoleHandler();
+ logger.addHandler(ch);
+ //SimpleFormatter sFormat = new SimpleFormatter();
+ XMLFormatter xml = new XMLFormatter();
+ //ch.setFormatter(sFormat);
+ ch.setFormatter(xml);
+ logger.log(Level.WARNING, "Тестовое логирование");
+ logger.info("Тестовое логирование");
+ }
+}
